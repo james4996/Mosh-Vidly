e1af584dee7d8b5ea1b51b90d555eea264e24ca5
James Walsh <james.walsh@assistivepartner.co.uk>
2019-09-20 15:40:43 +0100

    End Of Section
    Finished Attribute Routing

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -17,22 +17,5 @@ namespace Vidly.Controllers
 
             return View(movie);
         }
-
-        public ActionResult Edit(int id)
-        {
-            return Content("Id = " + id);
-        }
-
-        // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
     }
 }
