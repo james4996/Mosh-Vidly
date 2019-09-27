2ab2a0e6a45a43f6ba7b6365aa583a7603ebceec
James Walsh <james.walsh@assistivepartner.co.uk>
2019-09-27 15:41:27 +0100

    Finished Shortcut to PM Console

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,18 +4,49 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
 
-            var movie = new Movie() {Name = "Shrek!"};
+            var movie = new Movie() {Name = "Shrek!" };
 
-            return View(movie);
+            var customers = new List<Customer>
+            {
+                new Customer() { Id = 1, Name = "James" },
+                new Customer() { Id = 2, Name = "Bob" },
+                new Customer() { Id = 3, Name = "Mark" }
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
     }
 }
